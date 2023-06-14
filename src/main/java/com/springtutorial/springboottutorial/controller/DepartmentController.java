2d8368a4441f04fd8088689ec6f1270a743bc3b0
Krishna Sameer <krishnasameer51@gmail.com>
2023-06-14 08:55:16 +0530

    CRUD ADDED

--- src/main/java/com/springtutorial/springboottutorial/controller/DepartmentController.java
+++ src/main/java/com/springtutorial/springboottutorial/controller/DepartmentController.java
@@ -2,11 +2,10 @@ package com.springtutorial.springboottutorial.controller;
 
 import com.springtutorial.springboottutorial.entity.Department;
 import com.springtutorial.springboottutorial.service.DepartmentService;
-import com.springtutorial.springboottutorial.service.DepartmentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -16,5 +15,23 @@ public class DepartmentController {
     public Department saveDepartment(@RequestBody Department department){
         return departmentService.saveDepartment(department);
 
+    }
+    @GetMapping("/departments")
+    public List<Department> fetchDepartmentList(){
+        return departmentService.fetchDepartmentList();
+    }
+    @GetMapping("/departments/{id}")
+    public Department fetchDepartmentsById(@PathVariable("id") Long departmentId){
+        return departmentService.fetchDepartmentById(departmentId);
+    }
+
+    @DeleteMapping("/departments/{id}")
+    public String deleteDepartmentById(@PathVariable("id") Long departmentId){
+        departmentService.deleteDepartmentById(departmentId);
+        return "Department Delete Successfully!";
+    }
+    @PutMapping("/departments/{id}")
+    public Department updateDepartment(@PathVariable("id") Long departmentId, @RequestBody Department department){
+        return departmentService.updateDepartment(departmentId,department);
     }
 }
--- src/main/java/com/springtutorial/springboottutorial/service/DepartmentService.java
+++ src/main/java/com/springtutorial/springboottutorial/service/DepartmentService.java
@@ -2,6 +2,16 @@ package com.springtutorial.springboottutorial.service;
 
 import com.springtutorial.springboottutorial.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
    public Department saveDepartment(Department department);
+
+  public List<Department> fetchDepartmentList();
+
+   public Department fetchDepartmentById(Long departmentId);
+
+   public void deleteDepartmentById(Long departmentId);
+
+   public Department updateDepartment(Long departmentId, Department department);
 }
